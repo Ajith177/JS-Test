8b03e7843b6473341807f05bdc360dc13ed44df6
Ajith <lonelover005@gmail.com>
2023-08-26 12:56:43 +0530

    initial commit

--- TASK-1/index.js
+++ TASK-1/index.js
@@ -1,29 +1,16 @@
-// function loadImage(url) {
-//     console.log(url);
-//     return new Promise((resolve, reject) => {
-//         let img = document.createElement('img');
-//         img.addEventListener('load', e => resolve(img));
-//         img.addEventListener('error', (e) => {
-//             reject(new Error('image is failed to load'))
-//         });
-//         // img.src = url;
-//     })
-// }
-// loadImage('https://fakestoreapi.com/products/')
-//     .then(img => document.getElementById("image-container")
-//         .appendChild(img))
-//     .catch(error => console.error(error));
-
+let submitting_btn=document.querySelector('#submitting_btn');
 let store = document.querySelector('.image-container')
 fetch(`https://fakestoreapi.com/products/`)
 .then((response)=>response.json())
 .then((response) =>{
-    response.map((value)=>{
-        console.log(value.image)
-        let img = document.createElement('img')
-        img.src = value.image
-        store.appendChild(img)
+    submitting_btn.addEventListener('click',(event)=>{
+        event.preventDefault();
+        response.map((value)=>{
+            console.log(value.image)
+            let img = document.createElement('img')
+            img.src = value.image
+            store.appendChild(img).setAttribute('class','javascript_images')
+        })
     })
-})
-
-// fetch('https://fakestoreapi.com/products/').then((response)=>response.json()).then(response)
+   
+});
